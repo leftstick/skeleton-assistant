6b17eb1e60242b9c9448fac1bc70695b21db41ce
Howard.Zuo <leftstick@qq.com>
2015-12-09 11:13:25 +0800

    reactor with class

--- test/basic.js
+++ test/basic.js
@@ -1,3 +1,4 @@
+'use strict';
 var ass = require('../');
 var should = require('should');
 var path = require('path');
@@ -20,7 +21,7 @@ describe('basic test', function() {
     it('simple generate CommonFeature', function(done) {
         var destPath = path.resolve(dest, 'Alert.js');
         ass({name: 'alert', dest: dest});
-        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *  Defines the alert\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n */\n\'use strict\';\nimport Base from \'lib/FeatureBase\';\n\nclass Feature extends Base {\n    constructor() {\n        super(\'alert\');\n    }\n\n    beforeStart() {}\n\n    run() {}\n}\n\nexport default Feature;\n', 'content is incorrect');
+        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *  Defines the alert\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n */\n\'use strict\';\nimport FeatureBase from \'lib/FeatureBase\';\n\nclass Feature extends FeatureBase {\n    constructor() {\n        super(\'alert\');\n    }\n\n    beforeStart() {}\n\n    execute() {}\n}\n\nexport default Feature;\n', 'content is incorrect');
         done();
     });
 
@@ -32,7 +33,7 @@ describe('basic test', function() {
             sourceType: 'LogicalController',
             model: 'commonjs'
         });
-        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *  Defines the HomeController controller\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n */\n\'use strict\';\nvar HomeController = function($scope) {\n\n    $scope.$on(\'$destroy\', function() {});\n};\n\nmodule.exports = [ \'$scope\', HomeController ];\n', 'content is incorrect');
+        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *  Defines the HomeController controller\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n */\n\'use strict\';\nvar HomeController = function($scope) {\n\n    $scope.$on(\'$destroy\', function() {});\n};\n\nHomeController.$inject = [\'$scope\'];\n\nmodule.exports = HomeController;\n', 'content is incorrect');
         done();
     });
 
@@ -44,7 +45,7 @@ describe('basic test', function() {
             sourceType: 'LogicalMain',
             model: 'amd'
         });
-        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *\n *   Defines a home feature\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n */\ndefine([\'lib/FeatureBase\'], function(Base) {\n    \'use strict\';\n\n    var Feature = function() {\n        Base.call(this, \'home\');\n    };\n\n    Feature.prototype = new Base();\n\n    Feature.prototype.constructor = Feature;\n\n    Feature.prototype.run = function() {};\n\n    return Feature;\n});\n', 'content is incorrect');
+        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *\n *   Defines a home feature\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n **/\n\'use strict\';\ndefine([\'lib/FeatureBase\'], function(FeatureBase) {\n\n    class Feature extends FeatureBase {\n\n        constructor() {\n            super(\'home\');\n        }\n\n        execute() {\n        }\n    }\n\n    return Feature;\n});\n', 'content is incorrect');
         done();
     });
 
@@ -56,7 +57,7 @@ describe('basic test', function() {
             sourceType: 'Routes',
             model: 'commonjs'
         });
-        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *\n *  Routes module expose route information used in home feature\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n */\n\'use strict\';\nvar tpl = require(\'./partials/home.html\');\n\nmodule.exports = [\n    {\n        id: \'home\',\n        isDefault: false,\n        when: \'/home\',\n        controller: \'HomeController\',\n        template: tpl\n    }\n];\n', 'content is incorrect');
+        should(fs.readFileSync(destPath, {encoding: 'utf8'})).eql('/**\n *\n *  Routes module expose route information used in home feature\n *\n *  @author  ' + username + '\n *  @date    ' + date + '\n *\n **/\n\'use strict\';\nvar tpl = require(\'./partials/home.html\');\n\nmodule.exports = [\n    {\n        id: \'home\',\n        isDefault: false,\n        when: \'/home\',\n        controller: \'HomeController\',\n        template: tpl\n    }\n];\n', 'content is incorrect');
         done();
     });
 
